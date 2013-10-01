f1df3f10ec8976115268aded0fababb8916e4b9d
Gareth Leachman <grleachman@yahoo.com>
2013-10-01 14:15:01 +0100

    Test weekends from on to off

--- specs/fullCalendarSpecs-options-weekends.js
+++ specs/fullCalendarSpecs-options-weekends.js
@@ -4,7 +4,7 @@ describe("when weekends option is set",function(){
 		affix("#calendar");
 		var cal = $("#calendar");
 	})
-	it("should show sat and sun if true",function(){
+	iit("should show sat and sun if true",function(){
 		var options = { weekends:true };
 		$("#calendar").fullCalendar(options);
 		var sun = $(".fc-day-header.fc-sun")[0];
@@ -13,7 +13,7 @@ describe("when weekends option is set",function(){
 		expect(sun).toBeDefined();
 		expect(sat).toBeDefined();
 	});
-	it("should not show sat and sun if false",function(){
+	iit("should not show sat and sun if false",function(){
 		var options = { weekends:false };	
 		$("#calendar").fullCalendar(options);
 		
@@ -22,4 +22,33 @@ describe("when weekends option is set",function(){
 		expect(sun).not.toBeDefined();
 		expect(sat).not.toBeDefined();
 	});
+	
+	describe(" and then changed",function(){
+		iit("should be able to switch the weekends from on to off",function(){
+			var options = { weekends:true };	
+			var newOptions = {weekends:false };
+			$("#calendar").fullCalendar(options);
+			var sunOn = $(".fc-day-header.fc-sun").length;
+			
+			$("#calendar").fullCalendar('options',newOptions);
+			var sunOff = $(".fc-day-header.fc-sun").length;
+			
+			expect(sunOn).toEqual(1);
+			expect(sunOff).toEqual(0);
+		})
+		iit("should be able to switch the weekends from off to on",function(){
+			var options = { weekends:false };	
+			var newOptions = {weekends:true };
+			$("#calendar").fullCalendar(options);
+			var sunOff = $(".fc-day-header.fc-sun").length;
+			
+			$("#calendar").fullCalendar('options',newOptions);
+			var sunOn = $(".fc-day-header.fc-sun").length;
+			
+			expect(sunOn).toEqual(1);
+			expect(sunOff).toEqual(0);
+			
+		})
+	});
+	
 });
