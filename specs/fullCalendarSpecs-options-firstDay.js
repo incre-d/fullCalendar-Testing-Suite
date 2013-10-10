d18cd87b292f5773fe43fbe4c17778f0e6f0321f
Gareth Leachman <grleachman@yahoo.com>
2013-10-10 23:23:48 +0100

    added more options testing.

--- specs/fullCalendarSpecs-options-firstDay.js
+++ specs/fullCalendarSpecs-options-firstDay.js
@@ -21,8 +21,15 @@ describe("First Day : ",function(){
 		});
 		
 		it("should make Sunday the first day of the week",function(){				
-			var dayFirst = $(".fc-day-header")[0];
-			expect(dayFirst).toHaveClass('fc-sun');
+			var daysOfWeek = $(".fc-day-header");
+			
+			expect(daysOfWeek[0]).toHaveClass('fc-sun');
+			expect(daysOfWeek[1]).toHaveClass('fc-mon');
+			expect(daysOfWeek[2]).toHaveClass('fc-tue');
+			expect(daysOfWeek[3]).toHaveClass('fc-wed');
+			expect(daysOfWeek[4]).toHaveClass('fc-thu');
+			expect(daysOfWeek[5]).toHaveClass('fc-fri');
+			expect(daysOfWeek[6]).toHaveClass('fc-sat');
 		});
 	});
 	describe("when setting firstDay to 1",function(){
@@ -32,8 +39,15 @@ describe("First Day : ",function(){
 		});
 		
 		it("should make Monday the first day of the week",function(){				
-			var dayFirst = $(".fc-day-header")[0];
-			expect(dayFirst).toHaveClass('fc-mon');
+			var daysOfWeek = $(".fc-day-header");
+			
+			expect(daysOfWeek[0]).toHaveClass('fc-mon');
+			expect(daysOfWeek[1]).toHaveClass('fc-tue');
+			expect(daysOfWeek[2]).toHaveClass('fc-wed');
+			expect(daysOfWeek[3]).toHaveClass('fc-thu');
+			expect(daysOfWeek[4]).toHaveClass('fc-fri');
+			expect(daysOfWeek[5]).toHaveClass('fc-sat');
+			expect(daysOfWeek[6]).toHaveClass('fc-sun');
 		});
 	});
 	describe("when setting firstDay to 2",function(){
@@ -43,8 +57,15 @@ describe("First Day : ",function(){
 		});
 		
 		it("should make Tuesday the first day of the week",function(){				
-			var dayFirst = $(".fc-day-header")[0];
-			expect(dayFirst).toHaveClass('fc-tue');
+			var daysOfWeek = $(".fc-day-header");
+			
+			expect(daysOfWeek[0]).toHaveClass('fc-tue');
+			expect(daysOfWeek[1]).toHaveClass('fc-wed');
+			expect(daysOfWeek[2]).toHaveClass('fc-thu');
+			expect(daysOfWeek[3]).toHaveClass('fc-fri');
+			expect(daysOfWeek[4]).toHaveClass('fc-sat');
+			expect(daysOfWeek[5]).toHaveClass('fc-sun');
+			expect(daysOfWeek[6]).toHaveClass('fc-mon');
 		});
 	});
 	describe("when setting firstDay to 3",function(){
@@ -54,8 +75,15 @@ describe("First Day : ",function(){
 		});
 		
 		it("should make Wednesday the first day of the week",function(){				
-			var dayFirst = $(".fc-day-header")[0];
-			expect(dayFirst).toHaveClass('fc-wed');
+			var daysOfWeek = $(".fc-day-header");
+			
+			expect(daysOfWeek[0]).toHaveClass('fc-wed');
+			expect(daysOfWeek[1]).toHaveClass('fc-thu');
+			expect(daysOfWeek[2]).toHaveClass('fc-fri');
+			expect(daysOfWeek[3]).toHaveClass('fc-sat');
+			expect(daysOfWeek[4]).toHaveClass('fc-sun');
+			expect(daysOfWeek[5]).toHaveClass('fc-mon');
+			expect(daysOfWeek[6]).toHaveClass('fc-tue');
 		});
 	});
 	describe("when setting firstDay to 4",function(){
@@ -65,8 +93,15 @@ describe("First Day : ",function(){
 		});
 		
 		it("should make Thursday the first day of the week",function(){				
-			var dayFirst = $(".fc-day-header")[0];
-			expect(dayFirst).toHaveClass('fc-thu');
+			var daysOfWeek = $(".fc-day-header");
+			
+			expect(daysOfWeek[0]).toHaveClass('fc-thu');
+			expect(daysOfWeek[1]).toHaveClass('fc-fri');
+			expect(daysOfWeek[2]).toHaveClass('fc-sat');
+			expect(daysOfWeek[3]).toHaveClass('fc-sun');
+			expect(daysOfWeek[4]).toHaveClass('fc-mon');
+			expect(daysOfWeek[5]).toHaveClass('fc-tue');
+			expect(daysOfWeek[6]).toHaveClass('fc-wed');
 		});
 	});
 	describe("when setting firstDay to 5",function(){
@@ -76,8 +111,15 @@ describe("First Day : ",function(){
 		});
 		
 		it("should make Friday the first day of the week",function(){				
-			var dayFirst = $(".fc-day-header")[0];
-			expect(dayFirst).toHaveClass('fc-fri');
+			var daysOfWeek = $(".fc-day-header");
+			
+			expect(daysOfWeek[0]).toHaveClass('fc-fri');
+			expect(daysOfWeek[1]).toHaveClass('fc-sat');
+			expect(daysOfWeek[2]).toHaveClass('fc-sun');
+			expect(daysOfWeek[3]).toHaveClass('fc-mon');
+			expect(daysOfWeek[4]).toHaveClass('fc-tue');
+			expect(daysOfWeek[5]).toHaveClass('fc-wed');
+			expect(daysOfWeek[6]).toHaveClass('fc-thu');
 		});
 	});
 	describe("when setting firstDay to 6",function(){
@@ -87,8 +129,15 @@ describe("First Day : ",function(){
 		});
 		
 		it("should make Saturday the first day of the week",function(){				
-			var dayFirst = $(".fc-day-header")[0];
-			expect(dayFirst).toHaveClass('fc-sat');
+			var daysOfWeek = $(".fc-day-header");
+			
+			expect(daysOfWeek[0]).toHaveClass('fc-sat');
+			expect(daysOfWeek[1]).toHaveClass('fc-sun');
+			expect(daysOfWeek[2]).toHaveClass('fc-mon');
+			expect(daysOfWeek[3]).toHaveClass('fc-tue');
+			expect(daysOfWeek[4]).toHaveClass('fc-wed');
+			expect(daysOfWeek[5]).toHaveClass('fc-thu');
+			expect(daysOfWeek[6]).toHaveClass('fc-fri');
 		});
 	});
 	
